2edee935d2de194acbcb05338db32d3b4618ee6b
emad <razavioo@gmail.com>
2025-02-04 13:20:09 +0300

    improve index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/baghdad-intertantional-fair-2025/',
   build: {
     outDir: 'docs',
   },
